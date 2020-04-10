2f2d06463be9cc7684bf27aa5fdf6fee9b6fa1de
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 01:02:38 -0700

    create guesage method

--- Simple_Chatty_Bot1.java
+++ Simple_Chatty_Bot1.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-class Simple_Chatty_Bot1{
+class Simple_Chatty_Bot1 {
     final static Scanner scanner = new Scanner(System.in);
+
     static void greet(String assistantName, String birthYear) {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
     }
+
     static void remindName() {
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
     }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
     }
+}
 
 
 
