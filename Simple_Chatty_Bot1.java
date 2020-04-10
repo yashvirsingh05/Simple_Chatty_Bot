fa2d50b6077f6add7c55cc063d1659e11e25453f
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 00:31:05 -0700

    method greet

--- Simple_Chatty_Bot1.java
+++ Simple_Chatty_Bot1.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 class Simple_Chatty_Bot1{
     final static Scanner scanner = new Scanner(System.in);
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
+
 }
