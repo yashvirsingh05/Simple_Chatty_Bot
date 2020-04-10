499d970d7de0fc9f30f02c71d1321193cc097c6b
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 01:08:24 -0700

    print the statements

--- Simple_Chatty_Bot1.java
+++ Simple_Chatty_Bot1.java
@@ -30,7 +30,14 @@ class Simple_Chatty_Bot1 {
             System.out.printf("%d!\n", i);
         }
     }
-    static void test() {}
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+    }
 }
 
 
